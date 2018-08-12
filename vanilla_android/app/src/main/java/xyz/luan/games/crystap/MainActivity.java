8a9312995272dd2bfe4b3c2f41b845d632e68477
Luan Nico <luanpotter27@gmail.com>
2018-08-12 18:03:37 -0300

    working on vanilla

--- vanilla_android/app/src/main/java/xyz/luan/games/crystap/MainActivity.java
+++ vanilla_android/app/src/main/java/xyz/luan/games/crystap/MainActivity.java
@@ -1,52 +1,87 @@
 package xyz.luan.games.crystap;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            }
+        });
+        fab.hide();
+
+        SignInButton b = new SignInButton(this);
+        RelativeLayout.LayoutParams layout = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layout.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        addContentView(b, layout);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSignIn();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void startSignIn() {
+        GoogleSignInOptions opts = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+//                .requestServerAuthCode("309817578324")
+//                .requestEmail()
+                .build();
+        GoogleSignInClient signInClient = GoogleSignIn.getClient(this, opts);
+        Intent intent = signInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount signedInAccount = result.getSignInAccount();
+                signedIn(signedInAccount);
+            } else {
+                String message = result.getStatus().getStatusMessage();
+                System.out.println(result.getStatus().getStatusCode());
+                System.out.println(result.getStatus().getStatusMessage());
+                System.out.println(result.getStatus().getResolution());
+                if (message == null || message.isEmpty()) {
+                    message = "Unexpected error " + result.getStatus();
+                }
+                new AlertDialog.Builder(this).setMessage(message).setNeutralButton(android.R.string.ok, null).show();
+            }
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void signedIn(GoogleSignInAccount account) {
+        System.out.println("------------------------------------------");
+        System.out.println(account);
+        System.out.println("------------------------------------------");
+        fab.show();
     }
 }
